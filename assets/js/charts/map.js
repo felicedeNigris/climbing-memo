05e96a54b23d91516964eb7c29139a0dff6fbf38
cmizony <cmizony@gmail.com>
2015-02-28 09:56:54 -0800

    Add set of new icons

--- assets/js/charts/map.js
+++ assets/js/charts/map.js
@@ -13,26 +13,36 @@ function getMapChart () {
 		for (var i=0 ; i < data.length ; i++) {
 			var site = data[i];
 
+			// Set content html
+
 			var contentString = '<div style="white-space:nowrap">';
 			contentString += '<b>'+site.name+'</b><br>';
+			contentString += '<ul class="list-unstyled" style="margin-bottom:0px">';
 
-			// List of climbing types
-			if (site.metrics.length > 0) {
-				contentString += '<ul class="list-unstyled" style="margin-bottom:0px">';
+			for (var j=0 ; j < site.metrics.length ; j++)
+				contentString += '<li>'+site.metrics[j].type+': '+site.metrics[j].count+'</li>';
 
-				for (var j=0 ; j < site.metrics.length ; j++)
-					contentString += '<li>'+site.metrics[j].type+': '+site.metrics[j].count+'</li>';
-
-				contentString += '</ul>';
-			}
-
+			contentString += '</ul>';
 			contentString += '</div>';
+
+			// Set marker icon
+			var markerIcon = "climbing_gray.png";
+			switch (site.metrics[0].type) {
+				case 'Sport lead':	markerIcon = "climbing_yellow.png"; break;
+				case 'Boulder':		markerIcon = "climbing_blue.png"; break;
+				case 'Traditional':	markerIcon = "climbing_green.png"; break;
+				case 'Multi-pitch':	markerIcon = "climbing_orange.png"; break;
+				case 'Top rope':	markerIcon = "climbing_gray.png"; break;
+			}
 
 			marker = new google.maps.Marker({
 				position: new google.maps.LatLng(site.latitude,site.longitude),
-				   map: map,
-				   title: site.name,
-				   infowindow: new google.maps.InfoWindow({content: contentString}),
+				map: map,
+				icon: 'assets/img/'+markerIcon,
+				title: site.name,
+				infowindow: new google.maps.InfoWindow({
+					content: contentString
+				}),
 			});
 
 			bounds.extend(marker.position);
