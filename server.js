3c8fe695c0ffd0cf9567c69dfb1ce0ac43ecb29b
cmizony <cmizony@gmail.com>
2015-03-06 23:17:58 -0800

    :green_heart: Update heroku port config to process env

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express();
 
+app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname));
 
-var server = app.listen(80,'climbing-memo.herokuapp.com');
+var server = app.listen(app.get('port'));
 
-console.log('Climbing Memo app listening');
+console.log('Climbing Memo app listening localhost at port ' + app.get('port'));
