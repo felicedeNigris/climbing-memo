90608ac5a7e0865610a5c7ba2fcc11e1edd8d9ca
Camille Mizony <cmizony@gmail.com>
2015-08-15 21:13:08 -0700

    :white_check_mark: Add tests for routes service

---
+++ test/spec/services/routessvc.js
@@ -0,0 +1,102 @@
+'use strict'
+
+describe('Service: routesSvc', function() {
+  beforeEach(module('climbingMemo'))
+  beforeEach(angular.mock.module('climbingMemo'))
+
+  beforeEach(angular.mock.module('climbingMemo', function($provide) {
+    $provide.constant('DATABASE_URL', 'test.mock/')
+  }))
+
+  var myService, httpBackend
+  beforeEach(inject(function(routesSvc, _$httpBackend_) {
+    myService = routesSvc
+    httpBackend = _$httpBackend_
+  }))
+
+  it('should GET /routes', function() {
+    httpBackend.expectGET('test.mock/routes.json').respond({ test: true })
+
+    myService.getRoutes().then(function(result) {
+      expect(result.data).toEqual({ test: true })
+    })
+    httpBackend.flush()
+  })
+
+  it('should GET /routes/:id', function() {
+    var id = 'myTestId'
+    httpBackend.expectGET('test.mock/routes/myTestId.json').respond({ test: true })
+
+    myService.getRoute(id).then(function(result) {
+      expect(result.data).toEqual({ test: true })
+    })
+    httpBackend.flush()
+  })
+
+  it('should add route POST /routes', function() {
+    var route = { name: 'test' }
+    httpBackend.expectPOST('test.mock/routes/.json', {
+      name: 'test'
+    }).respond(true)
+
+    myService.addRoute(route).then(function(result) {
+      expect(result.data).toEqual(true)
+    })
+    httpBackend.flush()
+  })
+
+  it('should clean route before adding and updating it', function() {
+    var id = 'myTestId'
+    var route = {
+      name:      'test',
+      '$testA':  true,
+      '$testB':  true
+    }
+    httpBackend.expectPOST('test.mock/routes/.json', {
+      json: { name: 'test' },
+      test: function(data) {
+        return _.isEqual(JSON.parse(data), { name: 'test' })
+      }
+    }).respond(true)
+    httpBackend.expectPATCH('test.mock/routes/myTestId.json', {
+      json: { name: 'test' },
+      test: function(data) {
+        return _.isEqual(JSON.parse(data), { name: 'test' })
+      }
+    }).respond(true)
+
+    myService.addRoute(route).then(function(result) {
+      expect(result.data).toEqual(true)
+    })
+    myService.updateRoute(route, id).then(function(result) {
+      expect(result.data).toEqual(true)
+    })
+
+    httpBackend.flush()
+  })
+
+  it('should delete route DELETE /routes/:id', function() {
+    var id = 'myTestId'
+    httpBackend.expectDELETE('test.mock/routes/myTestId.json').respond(true)
+
+    myService.deleteRoute(id).then(function(result) {
+      expect(result.data).toEqual(true)
+    })
+    httpBackend.flush()
+  })
+
+  it('should clean route before updating it', function() {
+  })
+
+  it('should update route PATCH /routes/:id', function() {
+    var id = 'myTestId'
+    var route = { name: 'test' }
+
+    httpBackend.expectPATCH('test.mock/routes/myTestId.json', {name: 'test'}).respond(true)
+
+    myService.updateRoute(route, id).then(function(result) {
+      expect(result.data).toEqual(true)
+    })
+    httpBackend.flush()
+  })
+})
